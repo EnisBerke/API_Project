be9db43705cd8382277ae06373dd39d89329e0d6
Enis Berke <enisberke09@hotmail.com>
2023-11-02 23:11:42 +0300

    Finished
    Finished

---
+++ Project_Final/Interface/IEmployees.cs
@@ -0,0 +1,14 @@
+﻿using Project_Final.Model;
+
+namespace Project_Final.Interface
+{
+    public interface IEmployees
+    {
+        public List<Employee> GetEmployeeDetails();
+        public Employee GetEmployeeDetails(int id);
+        public void AddEmployee(Employee employee);
+        public void UpdateEmployee(Employee employee);
+        public Employee DeleteEmployee(int id);
+        public bool CheckEmployee(int id);
+    }
+}
